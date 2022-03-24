74587b49f9668776d4184611de8575b130f86524
ori tikozki <oritikozki@gmail.com>
2022-03-24 22:39:17 +0200

    added variations

--- SnowBoardShopProject.DB/SnowBoardShopProject.DB/Models/Client.cs
+++ SnowBoardShopProject.DB/SnowBoardShopProject.DB/Models/Client.cs
@@ -33,29 +33,30 @@ namespace SnowBoardShopProject.DB.Models
 
 
 
-        public bool ValidateEmail(string mail)
+        public static bool ValidateEmail(string mail)
         {
             string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
             return Regex.Match(mail, pattern).Success;
         }
-        public bool ValidateName(string fullName)
+        public static bool ValidateName(string fullName)
         {
             string IsValidName = @"\D{2,18} \D{2,18}";
             return Regex.Match(fullName, IsValidName).Success;
         }
-        public bool ValidatePhone(string phone)
+        public static bool ValidatePhone(string phone)
         {
-            string Pattern = @"\+(9[976]\d | 8[987530]\d | 6[987]\d | 5[90]\d | 42\d | 3[875]\d | 2[98654321]\d | 9[8543210] | 8[6421] | 6[6543210] | 5[87654321] | 4[987654310] | 3[9643210] | 2[70] | 7 | 1)\d{ 1,14}$";
+            string Pattern = @"^[0-9]{10}$";
+
             return Regex.Match(phone, Pattern).Success;
         }
-        public bool ValidateUserName(string username)
+        public static bool ValidateUserName(string username)
         {
-            string pattern = @"^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$";
+            string pattern = "(?=.*?[a - z])(?=.*?[A - Z])(?=.*?[0 - 9])";
             return Regex.Match(pattern, username).Success;
         }
-        public bool ValidatePassword(string password)
+        public static bool ValidatePassword(string password)
         {
-            string pattern = @"^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$";
+            string pattern = "(?=.*?[a - z])(?=.*?[A - Z])(?=.*?[0 - 9])";
             return Regex.Match(pattern, password).Success;
         }
 
--- SnowBoardShopProject/SnowBoardShopProject/UserControls/ClientsUserControl.cs
+++ SnowBoardShopProject/SnowBoardShopProject/UserControls/ClientsUserControl.cs
@@ -40,37 +40,53 @@ namespace SnowBoardShopProject
             {
                 //Modole: Client. -עשיתי פה ולידציה על מספר פלאפון תקין,מס זהות תקין,שם ושם משפחה תקינים. ראה ולידציות ב
                 //here im using "Factory method" to Create a new client.
-                if (Client.IsEmailValid(EmailtextBox6.Text) && Client.IsFNameValid(FirstNametextBox3.Text + " " + LastNametextBox4.Text) && Client.IsDecimal(PhoneNumbertextBox5.Text))
+                if (Client.ValidateEmail(EmailtextBox6.Text) && Client.ValidateName(FirstNametextBox3.Text + " " + LastNametextBox4.Text) && Client.ValidatePhone(PhoneNumbertextBox5.Text) && Client.ValidatePassword(PasswordtextBox1.Text) && Client.ValidateUserName(UserNametextBox1.Text))
                 {
                     var newClient = Client.CreateClientInstance(FirstNametextBox3.Text, LastNametextBox4.Text, Convert.ToInt32(PhoneNumbertextBox5.Text), EmailtextBox6.Text, UserNametextBox1.Text, PasswordtextBox1.Text);
                    
                     db.Clients.Add(newClient);
                     
+                    db.SaveChanges();
                     MessageBox.Show("Client Added Succesfully");
+                    LoginForm loginForm = new LoginForm();
+                    this.Hide();
+                    loginForm.Dock = DockStyle.Fill;
+                    loginForm.BringToFront();
+                    this.Size = new System.Drawing.Size(528, 731);
+                    IsHide = true;
                 }
                 else
                 {
-                    if (!Client.IsEmailValid(EmailtextBox6.Text))
+                    if (!Client.ValidateEmail(EmailtextBox6.Text))
                     {
                         lblMessegeTxt.Text = "Email is not valid";
                         return;
                     }
-                    if (!Client.IsFNameValid(FirstNametextBox3.Text + " " + LastNametextBox4.Text))
+                    if (!Client.ValidateName(FirstNametextBox3.Text + " " + LastNametextBox4.Text))
                     {
                         lblMessegeTxt.Text = "Youre first name OR last name is not valid";
+                        return;
+                    }
+                    if(!Client.ValidatePhone(PhoneNumbertextBox5.Text))
+                    {
+                        lblMessegeTxt.Text = "Youre phone number is not valid";
+                        return;
+                    }
+                    if (!Client.ValidateUserName(UserNametextBox1.Text))
+                    {
+                        lblMessegeTxt.Text = "Youre username is not valid";
+                        return;
+                    }
+                    if (!Client.ValidatePassword(PasswordtextBox1.Text))
+                    {
+                        lblMessegeTxt.Text = "Youre password is not valid";
                         return;
                     }
                     lblMessegeTxt.Text = "one or more of the following information is not valid";
 
                     
                 }
-                db.SaveChanges();
-                LoginForm loginForm = new LoginForm();
-                this.Hide();
-                loginForm.Dock = DockStyle.Fill;
-                loginForm.BringToFront();
-                this.Size = new System.Drawing.Size(528, 731);
-                IsHide = true;
+                
             }
         }
     }

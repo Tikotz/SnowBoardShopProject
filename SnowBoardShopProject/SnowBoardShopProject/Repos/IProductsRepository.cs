8661814805b3a3c066e3380fc4610093e648722e
ori tikozki <oritikozki@gmail.com>
2022-04-10 15:13:42 +0300

    last update

--- SnowBoardShopProject/SnowBoardShopProject/Repos/IProductsRepository.cs
+++ SnowBoardShopProject/SnowBoardShopProject/Repos/IProductsRepository.cs
@@ -3,8 +3,6 @@ using SnowBoardShopProject.DB.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SnowBoardShopProject.Repos
 {
